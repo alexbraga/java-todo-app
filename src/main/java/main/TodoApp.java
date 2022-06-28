76fbdc1ab1cae19ba7c3b62fa9b816e80cb7e471
Alex Braga <contato@alexbraga.com.br>
2022-06-27 21:06:48 -0300

    Rename 'Main.java' to 'TodoApp.java' and start user interface

---
+++ src/main/java/main/TodoApp.java
@@ -0,0 +1,12 @@
+package main;
+
+import view.MainScreen;
+
+import java.sql.SQLException;
+
+public class TodoApp {
+    public static void main(String[] args) throws SQLException {
+        // Start user interface
+        MainScreen.main(null);
+    }
+}
